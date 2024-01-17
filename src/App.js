b5fd3a1bfd89850d2ed180fd845776a1f0e9ac36
andrew03meyer <128401407+andrew03meyer@users.noreply.github.com>
2024-01-17 14:17:02 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation";
 
-import {
-    BrowserRouter as Router, Route, Switch
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <Navigation />
-
+        {/*<Header />*/}
       <Router>
-          <Switch>
+          <Navigation />
+          
+          <Routes>
               <Route path="/" exact component={Header} />
               <Route path="/about" component={AboutMe} />
-          </Switch>
+          </Routes>
       </Router>
     </div>
   );
