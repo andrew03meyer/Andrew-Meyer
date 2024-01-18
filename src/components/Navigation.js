8ca0aeb7986a18944e0ade8e05bdc2f1872ac850
andrew03meyer <128401407+andrew03meyer@users.noreply.github.com>
2024-01-18 11:52:03 +0000

    Added am logo to nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Navigation.css';
+import "./Navigation.css";
+import AMlogo from "../../assets/images/logo-svg.svg";
 
 const Navigation = () => {
   return (
     <nav>
       <ul>
-        <li><Link to="/Andrew-Meyer/">Home</Link></li>
-        <li><Link to="/Andrew-Meyer/about">About Me</Link></li>
+        <li>
+          <img src={AMlogo} alt="AM logo"></img>
+        </li>
+        <li>
+          <Link to="/Andrew-Meyer/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Andrew-Meyer/about">About Me</Link>
+        </li>
       </ul>
     </nav>
   );
