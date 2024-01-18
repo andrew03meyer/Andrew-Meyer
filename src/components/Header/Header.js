d2bcaaa373fb88ce5c3ab3b96cc86496f31af485
andrew03meyer <128401407+andrew03meyer@users.noreply.github.com>
2024-01-18 13:42:57 +0000

    Code regig, added photos

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,38 @@
-import LinkedinLogo from "../../assets/images/linkedin_logo.webp";
-import GitHubLogo from "../../assets/images/github_logo.svg";
+import Working from "../../assets/images/working.jpg";
 import PfP from "../../assets/images/id_photo.jpg";
-import FacebookLogo from "../../assets/images/Facebook_logo.svg";
 import React from "react";
+import './Header.css';
 
 function Header() {
   return (
-      <div className={"profile"}>
-            <img id="id_photo" src={PfP} alt="ID Photo"></img>
-            <img id="id_photo" src={PfP} alt="ID Photo"></img>
-            <img id="id_photo" src={PfP} alt="ID Photo"></img>
-            <img id="id_photo" src={PfP} alt="ID Photo"></img>
-          {/*</div>*/}
+    <div className={"profile"}>
+      <img id="id_photo" src={PfP} alt="ID Photo"></img>
+      <img id="id_photo" src={PfP} alt="ID Photo"></img>
+      <img id="id_photo" src={PfP} alt="ID Photo"></img>
+      <img id="id_photo" src={Working} alt="ID Photo"></img>
+      {/*</div>*/}
 
-          <span className={"contents"}>
-              <h2>About me:</h2>
-              <p>
-                  I'm currently studying at the University of Kent, in my second year.
-                  Currently, I'm mainly focused in Web Development, Computer Systems,
-                  AI, Correctness and Efficiency of Algorithms. I'm a dedicated ringer,
-                  of 15 years, and an avid tennis player. I've had a passion for
-                  technology since I was young, and have funneled that into my studies
-                  and spare time.
-              </p>
-          </span>
-          <span className={"contents"}>
-              <h2>Opportunities:</h2>
-              <p>
-                  I'm currently looking for a year in industry for (Jun 2024 - Jul 2025)
-                  but also open for anything that will develop my skills. I enjoy data
-                  analysis as well as web development, but I am open to experience any
-                  type of technological opportunity.
-              </p>
-          </span>
-      </div>
+      <span className={"contents"}>
+        <h2>About me:</h2>
+        <p>
+          I'm currently studying at the University of Kent, in my second year.
+          Currently, I'm mainly focused in Web Development, Computer Systems,
+          AI, Correctness and Efficiency of Algorithms. I'm a dedicated ringer,
+          of 15 years, and an avid tennis player. I've had a passion for
+          technology since I was young, and have funneled that into my studies
+          and spare time.
+        </p>
+      </span>
+      <span className={"contents"}>
+        <h2>Opportunities:</h2>
+        <p>
+          I'm currently looking for a year in industry for (Jun 2024 - Jul 2025)
+          but also open for anything that will develop my skills. I enjoy data
+          analysis as well as web development, but I am open to experience any
+          type of technological opportunity.
+        </p>
+      </span>
+    </div>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,24 +13,28 @@ const Navigation = () => {
         <li>
           <img className="logo" src={AMlogo} alt="AM logo"></img>
         </li>
-        <li className="name">
-          Andrew Meyer
-        </li>
+        <li className="name">Andrew Meyer</li>
         <li>
           <Link to="/Andrew-Meyer/">Home</Link>
         </li>
         <li>
           <Link to="/Andrew-Meyer/about">About Me</Link>
         </li>
-          <li className={"socials"} style={{marginLeft: "auto"}}><a href="https://github.com/andrew03meyer">
+        <li className={"socials"} style={{ marginLeft: "auto" }}>
+          <a href="https://github.com/andrew03meyer">
             <img className="logo" src={GitHubLogo} alt="github"></img>
-          </a></li>
-          <li className={"socials"}><a href="https://www.linkedin.com/in/andrew-meyer-200103266/">
+          </a>
+        </li>
+        <li className={"socials"}>
+          <a href="https://www.linkedin.com/in/andrew-meyer-200103266/">
             <img className="logo" src={LinkedinLogo} alt="linkedin"></img>
-          </a></li>
-          <li className={"socials"}><a href="https://www.facebook.com/andrew03meyer/">
+          </a>
+        </li>
+        <li className={"socials"}>
+          <a href="https://www.facebook.com/andrew03meyer/">
             <img className="logo" src={FacebookLogo} alt="Facebook"></img>
-          </a></li>
+          </a>
+        </li>
       </ul>
     </nav>
   );
