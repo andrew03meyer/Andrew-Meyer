c7600b7abd9bb6a6e0ebba27bdff2104ebc54fa1
andrew03meyer <128401407+andrew03meyer@users.noreply.github.com>
2024-01-18 11:26:12 +0000

    Added navbar
    changed current links to navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Navigation.css';
 
 const Navigation = () => {
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/Andrew-Meyer/">Home</Link>
-        </li>
-        <li>
-          <Link to="/Andrew-Meyer/about">About Me</Link>
-        </li>
+        <li><Link to="/Andrew-Meyer/">Home</Link></li>
+        <li><Link to="/Andrew-Meyer/about">About Me</Link></li>
       </ul>
     </nav>
   );
