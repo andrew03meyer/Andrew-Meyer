a1a91b775030d88348df1ac1e25392e1559d67e0
Andrew Meyer <am2660@kent.ac.uk>
2024-01-19 16:31:07 +0000

    PhotoCollage
    + shading and text on hover
    - XML for ringing

--- src/components/Ringing/Ringing.js
+++ src/components/Ringing/Ringing.js
@@ -1,21 +1,7 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./Ringing.css";
-function Ringing() {
-
-    let url = "https://bb.ringingworld.co.uk/view.php?id=1209822";
-
-    fetch(url, {method:"GET", headers: "Accept", "application/html"})
-        .then(response => response.text())
-        .then(xmlData => {
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(xmlData);
-            const xmlContent = xmlDoc.documentElement.outerHTML;
-
-            return(
-                <p>testing</p>
-            )
-        })
-        .catch(error => console.log("eeror"));
+// import PHP from ''
+function Ringing(){
 }
 
 export default Ringing;
