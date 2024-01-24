c52102c7ff8e3e7077cbfefb6fd77f3c5ca8b337
andrew03meyer <128401407+andrew03meyer@users.noreply.github.com>
2024-01-24 15:02:58 +0000

    Updated nav bar for links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -15,19 +15,19 @@ const Navigation = () => {
           <img className="logo" src={AMlogo} alt="AM logo"></img>
         </li>
         <li>
-          <Link to="/Andrew-Meyer/" className={"name"}>
+          <Link to="/" className={"name"}>
             Andrew Meyer
           </Link>
         </li>
 
         <li>
-          <Link to="/Andrew-Meyer/">Home</Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/Andrew-Meyer/ringing">Ringing</Link>
+          <Link to="/ringing">Ringing</Link>
         </li>
         <li>
-          <Link to="/Andrew-Meyer/gallery">Gallery</Link>
+          <Link to="/gallery">Gallery</Link>
         </li>
 
         <li className={"socials"} style={{ marginLeft: "auto" }}>
