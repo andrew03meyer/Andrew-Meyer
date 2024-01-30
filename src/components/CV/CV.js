76ff328a5c76bf4159170194ece2d02277df7147
andrew03meyer <128401407+andrew03meyer@users.noreply.github.com>
2024-01-30 13:15:26 +0000

    Indentation

--- src/components/CV/CV.js
+++ src/components/CV/CV.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import CV from '../../assets/CV.pdf';
+import React from "react";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import CV from "../../assets/CV.pdf";
 
 function CvView() {
-    return (
-        <div id={"content-t"}>
-            <p>view my cv:</p>
-            <Worker workerUrl={`https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js`}>
-                <Viewer fileUrl={CV} />
-            </Worker>
-        </div>
-    );
-};
+  return (
+    <div id={"content-t"}>
+      <p>view my cv:</p>
+      <Worker
+        workerUrl={`https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js`}
+      >
+        <Viewer fileUrl={CV} />
+      </Worker>
+    </div>
+  );
+}
 
 export default CvView;
