5c63679f027ef3fc7a93ad8a1588c0d5b8b80a15
andrew03meyer <128401407+andrew03meyer@users.noreply.github.com>
2024-02-01 16:02:11 +0000

    Update CV.js

--- src/components/CV/CV.js
+++ src/components/CV/CV.js
@@ -1,16 +1,16 @@
 import React from "react";
-import CV from "../../assets/CV.pdf"
-import "./CV.css"
-import Navbar from "../Navigation/Navigation"
+import CV from "../../assets/CV.pdf";
+import "./CV.css";
+import Navbar from "../Navigation/Navigation";
 import Background from "../Background/Background";
 
 const PDFViewer = () => {
-    return (
-        <>
-            <Background />
-            <Navbar />
-            <iframe className={"iframe-cv"} src={CV} />
-        </>
-    );
+  return (
+    <>
+      <Background />
+      <Navbar />
+      <iframe className={"iframe-cv"} src={CV} />
+    </>
+  );
 };
 export default PDFViewer;
