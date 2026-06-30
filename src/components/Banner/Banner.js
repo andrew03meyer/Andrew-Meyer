4c639b71fad4438596209e103c4ccb5f82c1b7f6
Andrew Meyer <am2660@kent.ac.uk>
2026-06-30 12:41:42 +0100

    Removed Job Banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,16 +2,17 @@ import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div className="banner" style={{ position: "relative" }}>
-      <h1>
-        I am currently looking for work after graduation in 2026. Please do get
-        in contact if you have opportunities available!
-      </h1>
-      <p>
-        Contact info in my CV or get in touch via the socials in the navigation
-        bar.
-      </p>
-    </div>
+    // <div className="banner" style={{ position: "relative" }}>
+    //   <h1>
+    //     I am currently looking for work after graduation in 2026. Please do get
+    //     in contact if you have opportunities available!
+    //   </h1>
+    //   <p>
+    //     Contact info in my CV or get in touch via the socials in the navigation
+    //     bar.
+    //   </p>
+    // </div>
+    <></>
   );
 };
 
